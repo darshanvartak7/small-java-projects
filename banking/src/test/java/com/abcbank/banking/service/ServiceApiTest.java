579b8403a5dc31b7d0bbec4273b26720677199fd
darshanvartak@yahoo.com <DarshanVartak@c02f98jdmd6m.lan>
2021-11-11 07:13:24 -0600

    Formatted

--- banking/src/test/java/com/abcbank/banking/service/ServiceApiTest.java
+++ banking/src/test/java/com/abcbank/banking/service/ServiceApiTest.java
@@ -9,57 +9,53 @@ import org.junit.jupiter.api.TestMethodOrder;
 import com.abcbank.banking.exceptions.AccountException;
 
 @TestMethodOrder(Alphanumeric.class)
-public class ServiceApiTest 
-{
+public class ServiceApiTest {
 	private static AccountService accountService;
-	
+
 	@BeforeClass
 	public static void setUp() {
-		accountService = new AccountServiceImpl(); 
+		accountService = new AccountServiceImpl();
 		accountService.loadAccountDetails(1234l);
 	}
 
-    @Test //validatePIN 
-    public void validateA(){
-    	assertTrue(accountService.validatePIN(1234l, 5656));
-    }
-    
-    @Test //validateBalance 
-    public void validateB(){
-    	assertTrue(accountService.getBalance() == 10000.60);
-    }
-    
-    @Test //validateDeposit
-    public void validateC(){
-    	try {
+	@Test // validatePIN
+	public void validateA() {
+		assertTrue(accountService.validatePIN(1234l, 5656));
+	}
+
+	@Test // validateBalance
+	public void validateB() {
+		assertTrue(accountService.getBalance() == 10000.60);
+	}
+
+	@Test // validateDeposit
+	public void validateC() {
+		try {
 			accountService.deposit(100);
 		} catch (AccountException e) {
 			e.printStackTrace();
 		}
-    	assertTrue(accountService.getBalance() == 10100.60);
-    }
-    
-    @Test //validateWithdrawal
-    public void validateD(){
-    	try {
+		assertTrue(accountService.getBalance() == 10100.60);
+	}
+
+	@Test // validateWithdrawal
+	public void validateD() {
+		try {
 			accountService.withdraw(100);
 		} catch (AccountException e) {
 			e.printStackTrace();
 		}
-    	assertTrue(accountService.getBalance() == 10000.60);
-    }
- 
-    @Test //validateInvalidAmount
-    public void validateE(){
-    	try {
+		assertTrue(accountService.getBalance() == 10000.60);
+	}
+
+	@Test // validateInvalidAmount
+	public void validateE() {
+		try {
 			accountService.withdraw(150000);
 		} catch (AccountException e) {
 			assertTrue(e.getMessage().equals("The account does not have sufficient balance"));
 		}
-    	
-    }
-    
-    
-    
-    
+
+	}
+
 }
